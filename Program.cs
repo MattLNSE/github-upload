575aa16daee8c8e804a5c80427dbf5711b06d59e
MattLNSE <Matthew.Bell@lexisnexisrisk.com>
2022-04-04 16:39:50 -0500

    Initial commit of test project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // To prevents the screen from running and closing quickly
+            Console.ReadKey();
+        }
+    }
+}
